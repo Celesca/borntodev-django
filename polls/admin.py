43e6a66c84bead618c4e09bd26d7b71c7ae34a08
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-08 19:28:15 +0700

    ✅ add: results.html

--- polls/admin.py
+++ polls/admin.py
@@ -6,3 +6,8 @@ from .models import Question
 
 # register Question to administrator
 admin.site.register(Question)
+# register Choice to administrator
+
+from .models import Choice
+
+admin.site.register(Choice)
